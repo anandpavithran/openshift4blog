e1392362f46771d2aea6c219ff26fddba2577e9b
root <root@localhost.localdomain>
2020-06-17 22:33:00 +0530

    June17

--- step-17.sh
+++ step-17.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 systemctl reload NetworkManager
 systemctl restart libvirtd
+ssh -i sshkey "lb.gls.test" "systemctl enable haproxy --now"
+ssh -i sshkey "lb.gls.test" "systemctl is-active haproxy"
 sleep 5
 virsh start gls-bootstrap
 virsh start gls-master
